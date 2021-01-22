39080e04f33880294510164204f54675950eae94
adeelpm <abdurrahmanadeel@gmail.com>
2021-01-22 08:08:10 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'brain.dart';
+
+void main() => runApp(Quizzler());
+
+QuizBrain qone = QuizBrain();
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  void check(bool x) {
+    setState(() {
+      if (qone.rqno() == qone.qlen() - 1) {
+        Alert(
+                context: context,
+                title: "RFLUTTER ALERT",
+                desc: "Flutter is more awesome with RFlutter Alert.")
+            .show();
+        qone.reset();
+        score.clear();
+      } else {
+        if (x) {
+          tr();
+        } else {
+          fa();
+        }
+        qone.nextQuestion();
+      }
+    });
+  }
+
+  void tr() {
+    return score.add(Icon(
+      Icons.check,
+      color: Colors.green,
+    ));
+  }
+
+  void fa() {
+    return score.add(Icon(
+      Icons.close,
+      color: Colors.red,
+    ));
+  }
+
+  List<Widget> score = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                qone.getq(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                check(qone.geta());
+                //The user picked true.
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+                check(!qone.geta());
+              },
+            ),
+          ),
+        ),
+        //TODO: Add a Row here as your sc ore keeper
+
+        Row(
+          children: score,
+        )
+      ],
+    );
+  }
+}
